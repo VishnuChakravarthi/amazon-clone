2a46fae59307d1e5f1e21b92d694a96ea36bfa08
VishnuChakravarthi <vishchakravarthy18@gmail.com>
2020-11-16 21:10:16 +0530

    Error correction before deployment

--- src/components/Payment/axios.js
+++ src/components/Payment/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5001/clone-c9d80/us-central1/api",
+  baseURL: "https://us-central1-clone-c9d80.cloudfunctions.net/api",
+  // baseURL: "http://localhost:5001/clone-c9d80/us-central1/api",
 });
+
+// https://us-central1-clone-c9d80.cloudfunctions.net/api
